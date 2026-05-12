79cfa6fe3e6651370496642089199ae26a3ca2a8
Khun <65932086+Maharahman@users.noreply.github.com>
2026-05-12 13:16:21 +0700

    Update 2026_04_05_044219_create_services_table.php

--- database/migrations/2026_04_05_044219_create_services_table.php
+++ database/migrations/2026_04_05_044219_create_services_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->string('name');
-            $table->int('price');
+            $table->integer('price');
         });
     }
 
